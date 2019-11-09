b95e9f8cf38df3692826b44853c94bc636359e17
jonathan.caine <jonathan.caine@peoplemedia.com>
2019-11-09 13:03:09 -0600

    final presentation and documentation

--- server.js
+++ server.js
@@ -13,4 +13,25 @@ app.get('/api/hello', (req, res) => {
   res.send({ value: callCount });
 });
 
+
+app.post('/api/postendpoint', function (req, res) {
+  console.log('the body', req.body);
+  let failed = false;
+  if (failed) {
+    res.status(500).end();
+  } else {
+    res.end();
+  }
+});
+
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+/* sample curl for the post
+curl -X POST
+  http://localhost:5000/api/postendpoint
+  -H 'Content - Length: 13' \
+  -H 'Content - Type: application / json' \
+  -H 'Host: localhost: 5000' \
+  -d '{ "test": 1 } '
+*/
